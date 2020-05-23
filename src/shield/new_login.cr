6fc49fa0b8a72985f02685f62bfb5497e25b3596
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2020-05-23 16:33:02 +0000

    Add missing example route definitions

--- src/shield/new_login.cr
+++ src/shield/new_login.cr
@@ -1,5 +1,9 @@
 module Shield::NewLogin
   macro included
+    # get "/log-in" do
+    #   html NewPage
+    # end
+
     skip :require_logged_in
     before :require_logged_out
   end
--- src/shield/new_password_reset.cr
+++ src/shield/new_password_reset.cr
@@ -1,5 +1,9 @@
 module Shield::NewPasswordReset
   macro included
+    # get "/password-resets/new" do
+    #   html NewPage
+    # end
+
     skip :require_logged_in
     before :require_logged_out
   end
