2d96c16e25d7656f97628e91e459caec95181919
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2020-05-28 21:38:57 +0000

    Introduce a top-level `Shield::Error` class
    All shield errors will inherit from this class.

---
+++ src/shield/error.cr
@@ -0,0 +1,2 @@
+class Shield::Error < Exception
+end
--- src/shield/not_authorized_error.cr
+++ src/shield/not_authorized_error.cr
@@ -1,15 +1,17 @@
-class Shield::NotAuthorizedError < Exception
-  getter user : User
-  getter action : AuthorizedAction
-  getter record : Model
+module Shield
+  class NotAuthorizedError < Error
+    getter user : User
+    getter action : AuthorizedAction
+    getter record : Model
 
-  def initialize(
-    @user,
-    @action,
-    @record,
-    @message = "Permission denied!",
-    @cause : Exception? = nil
-  )
-    super message, cause
+    def initialize(
+      @user,
+      @action,
+      @record,
+      @message = "Permission denied!",
+      @cause : Exception? = nil
+    )
+      super message, cause
+    end
   end
 end
