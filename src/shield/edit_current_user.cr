9d38ec274355873f1999930da5a16bb1bf0a964a
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2020-05-28 22:40:57 +0000

    Add edit and update actions for current user

---
+++ src/shield/edit_current_user.cr
@@ -0,0 +1,8 @@
+module Shield::EditCurrentUser
+  macro included
+    # get "/profile/edit" do
+    #   authorize(:update, current_user!)
+    #   html EditPage, user: current_user!
+    # end
+  end
+end
---
+++ src/shield/update_current_user.cr
@@ -0,0 +1,27 @@
+module Shield::UpdateCurrentUser
+  macro included
+    # patch "/profile" do
+    #   save_current_user
+    # end
+
+    private def save_current_user
+      SaveCurrentUser.update(current_user!, params) do |operation, updated_user|
+        if operation.saved?
+          success_action(operation, updated_user)
+        else
+          failure_action(operation, updated_user)
+        end
+      end
+    end
+
+    private def success_action(operation, user)
+      flash.success = "User updated successfully"
+      redirect to: Show
+    end
+
+    private def failure_action(operation, user)
+      flash.failure = "Could not update user"
+      html EditPage, operation: operation, user: user
+    end
+  end
+end
