7f8cb804d451a9c72406e04a0f3664fd6205d9bc
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2020-05-30 14:55:51 +0000

    Set `#user` to the current user for current user actions

--- src/shield/actions/edit_current_user.cr
+++ src/shield/actions/edit_current_user.cr
@@ -1,8 +1,12 @@
 module Shield::EditCurrentUser
   macro included
     # get "/profile/edit" do
-    #   authorize(:update, current_user!)
-    #   html EditPage, user: current_user!
+    #   authorize(:update, user)
+    #   html EditPage, user: user
     # end
+
+    private def user
+      current_user!
+    end
   end
 end
--- src/shield/actions/update_current_user.cr
+++ src/shield/actions/update_current_user.cr
@@ -1,17 +1,22 @@
 module Shield::UpdateCurrentUser
   macro included
     # patch "/profile" do
+    #   authorize(:update, user)
     #   save_current_user
     # end
 
     private def save_current_user
-      SaveCurrentUser.update(current_user!, params) do |operation, updated_user|
+      SaveCurrentUser.update(user, params) do |operation, updated_user|
         if operation.saved?
           success_action(operation, updated_user)
         else
           failure_action(operation, updated_user)
         end
       end
+    end
+
+    private def user
+      current_user!
     end
 
     private def success_action(operation, user)
