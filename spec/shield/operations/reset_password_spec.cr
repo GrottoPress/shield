c713273af827ac1acaba430f62d3eea15c580dcd
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2020-06-30 22:01:20 +0000

    Ensure password is always saved when resetting password
    `SavePassword` operation does not save password if the new
    password is same as the old. This is useful for ensuring the
    password hash is not changed, thus notification is not sent.
    
    In `ResetPassword`, we need the hash to change even if the new
    password is same as old, in order to trigger `after_save` and
    delete the reset token.
    
    We should probably find a better solution, though.

--- spec/shield/operations/reset_password_spec.cr
+++ spec/shield/operations/reset_password_spec.cr
@@ -59,4 +59,31 @@ describe Shield::ResetPassword do
         .should_not(be_nil)
     end
   end
+
+  it "deletes password reset token when new password equals old" do
+    email = "user@example.tld"
+    password = "password12U\\password"
+    new_password = password
+
+    SaveCurrentUser.create!(
+      email: email,
+      password: password,
+      password_confirmation: password,
+      login_notify: true,
+      password_notify: true
+    )
+
+    password_reset = SavePasswordReset.create!(user_email: email)
+
+    ResetPassword.update(
+      password_reset.user!,
+      password: new_password,
+      password_confirmation: new_password,
+      password_reset: password_reset
+    ) do |operation, updated_user|
+      operation.saved?.should be_true
+
+      PasswordResetQuery.find(password_reset.id).token_hash.should be_nil
+    end
+  end
 end
