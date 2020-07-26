cdb129d4a6af1bb7af7785c810d7f2341cbbdb30
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2020-07-26 23:19:57 +0000

    Fix wrong indentation

--- spec/app/config/server.cr
+++ spec/app/config/server.cr
@@ -1,8 +1,8 @@
 Lucky::Server.configure do |settings|
-    settings.secret_key_base = ENV["SECRET_KEY_BASE"]
-    settings.host = ENV["SERVER_HOST"]
-    settings.port = ENV["SERVER_PORT"].to_i
-    settings.gzip_enabled = false
+  settings.secret_key_base = ENV["SECRET_KEY_BASE"]
+  settings.host = ENV["SERVER_HOST"]
+  settings.port = ENV["SERVER_PORT"].to_i
+  settings.gzip_enabled = false
 end
 
 Lucky::ForceSSLHandler.configure do |settings|
