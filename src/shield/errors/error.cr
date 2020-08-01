f1d1eec73e62f115dd775d2d45e115f428b37c3e
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2020-08-01 13:56:50 +0000

    Use `Lucky::RenderableError` in `Shield::Error`

--- src/shield/errors/error.cr
+++ src/shield/errors/error.cr
@@ -1,2 +1,11 @@
 class Shield::Error < Exception
+  include Lucky::RenderableError
+
+  def renderable_status : Int32
+    403
+  end
+
+  def renderable_message : String
+    message.to_s
+  end
 end
--- src/shield/errors/no_authorization_error.cr
+++ src/shield/errors/no_authorization_error.cr
@@ -1,4 +1,7 @@
 module Shield
   class NoAuthorizationError < Error
+    def renderable_status : Int32
+      503
+    end
   end
 end
