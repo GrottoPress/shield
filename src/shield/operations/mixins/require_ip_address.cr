ca39895379447993580e354efe2fe0fd14aec337
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2020-08-12 15:06:28 +0000

    Refactor module

--- src/shield/operations/mixins/require_ip_address.cr
+++ src/shield/operations/mixins/require_ip_address.cr
@@ -3,13 +3,9 @@ module Shield::RequirIpAddress
     needs remote_ip : Socket::IPAddress?
 
     before_save do
-      require_ip_address
       set_ip_address
-    end
 
-    private def require_ip_address
-      return unless remote_ip.nil?
-      ip_address.add_error("could not be determined")
+      validate_required ip_address, message: "could not be determined"
     end
 
     private def set_ip_address
