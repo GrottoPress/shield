48fcf812b82aa2ab2d1f06a3b29ccc6e8228dc47
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2020-09-05 16:04:42 +0000

    Add more `Shield::CryptoHelper` specs

--- spec/shield/helpers/crypto_helper_spec.cr
+++ spec/shield/helpers/crypto_helper_spec.cr
@@ -1,6 +1,28 @@
 require "../../spec_helper"
 
 describe Shield::CryptoHelper do
+  describe ".verify_and_decrypt" do
+    it "verifies and decrypts ciphertext plaintext" do
+      plaintext = "abcdef"
+
+      ciphertext = CryptoHelper.encrypt_and_sign(plaintext)
+      decrypted = CryptoHelper.verify_and_decrypt!(ciphertext)
+
+      decrypted[0].should eq(plaintext)
+    end
+
+    it "verifies and decrypts ciphertext multiple plaintexts" do
+      plaintext = "abcdef"
+      plaintext_2 = "123456"
+
+      ciphertext = CryptoHelper.encrypt_and_sign(plaintext, plaintext_2)
+      decrypted = CryptoHelper.verify_and_decrypt!(ciphertext)
+
+      decrypted[0].should eq(plaintext)
+      decrypted[1].should eq(plaintext_2)
+    end
+  end
+
   describe ".verify_sha256?" do
     it "verifies unsalted SHA256" do
       plaintext = "abcdef"
