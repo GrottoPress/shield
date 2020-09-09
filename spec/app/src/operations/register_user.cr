556e8eda0f2e6e61668db54787e332bc55bb5977
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2020-09-09 19:10:04 +0000

    Add missing validations
    Since we have disabled default validations, we are required to
    explicitly define all validations.

--- spec/app/src/operations/register_user.cr
+++ spec/app/src/operations/register_user.cr
@@ -2,4 +2,8 @@ class RegisterUser < User::SaveOperation
   include Shield::RegisterUser
 
   permit_columns :level
+
+  before_save do
+    validate_required level
+  end
 end
--- spec/app/src/operations/update_user.cr
+++ spec/app/src/operations/update_user.cr
@@ -2,4 +2,8 @@ class UpdateUser < User::SaveOperation
   include Shield::UpdateUser
 
   permit_columns :level
+
+  before_save do
+    validate_required level
+  end
 end
