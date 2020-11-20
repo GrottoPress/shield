2a4836ca0312a109449fd149c089dcdd10349f68
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2020-11-20 19:50:19 +0000

    Revert "Remove redundant method"
    This commit reverts commit a257b0e1586d58b9697300df5764f9749ab7fef4.
    My bad, this caused compile errors in some cases.

--- src/charms.cr
+++ src/charms.cr
@@ -656,6 +656,10 @@ macro __enum(enum_name, &block)
       @raw = Raw{{ enum_name }}.parse(value)
     end
 
+    def initialize(value : Symbol)
+      @raw = initialize(value.to_s)
+    end
+
     delegate :to_s, to: @raw
     forward_missing_to @raw
 
