2489fe1d76b8d828594c16535786701b2d4d7f81
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2021-01-22 16:22:05 +0000

    Add `.needs` support to nested `.has_one` operations
    I've had to move the implementation to a new module, in order to
    have the our macro definition override the one in *Avram* core.

--- src/charms.cr
+++ src/charms.cr
@@ -131,8 +131,109 @@ module Avram
     include MailHelpers
   end
 
+  # Avram's implementation errors in an update operation:
+  #
+  # `duplicate key value violates unique constraint
+  # "<constraint name>" (PQ::PQError)`
+  #
+  # `{{ type }}.new(params)` causes `{{ name }}.save` to create
+  # (rather than update) a record each time it is called, since
+  # no record was passed when the nested operation was instantiated.
+  #
+  # Ref: https://github.com/luckyframework/avram/blob/efe1d8afaf337809c2accf51300daac48dba1cdc/src/avram/nested_save_operation.cr
+  module NewNestedSaveOperation
+    macro included
+      macro inherited
+        define_has_one
+      end
+    end
+
+    macro define_has_one
+      macro has_one(type_declaration)
+        \{% name = type_declaration.var %}
+        \{% type = type_declaration.type.resolve %}
+
+        \{% model_type = type.ancestors.find do |t|
+            t.stringify.starts_with?("Avram::SaveOperation(")
+          end.type_vars.first %}
+
+        \{% assoc = T.constant(:ASSOCIATIONS).find do |assoc|
+            assoc[:relationship_type] == :has_one &&
+              assoc[:type].resolve.name == model_type.name
+          end %}
+
+        \{% if ! assoc %}
+          \{% raise "#{T} must have a has_one association with #{model_type}" %}
+        \{% end %}
+
+        \{% for need in type.constant(:OPERATION_NEEDS) %}
+          needs \{{ name }}_\{{ need }}
+        \{% end %}
+
+        after_save save_\{{ name }}
+
+        def save_\{{ name }}(saved_record)
+          unless \{{ name }}.save
+            add_error(:\{{ name }}, "failed")
+            mark_nested_save_operations_as_failed
+            database.rollback
+          end
+        end
+
+        def \{{ name }}
+          @\{{ name }} ||= if new_record?
+            \{% begin %}\{{ type }}.new(
+              params,
+              \{% for need in type.constant(:OPERATION_NEEDS) %}
+                \{{ need.var }}: \{{ name }}_\{{ need.var }},
+              \{% end %}
+            )\{% end %}
+          else
+            \{% begin %}\{{ type }}.new(
+              record!.\{{ assoc[:assoc_name].id }}!,
+              params,
+              \{% for need in type.constant(:OPERATION_NEEDS) %}
+                \{{ need.var }}: \{{ name }}_\{{ need.var }},
+              \{% end %}
+            )\{% end %}
+          end
+
+          nested = @\{{ name }}.not_nil!
+
+          record.try do |record|
+            nested.\{{ @type.constant(:FOREIGN_KEY).id }}.value = record.id
+          end
+
+          nested
+        end
+
+        def nested_save_operations
+          \{% if @type.methods.map(&.name).includes?(:nested_save_operations.id) %}
+            previous_def +
+          \{% end %}
+          [\{{ name }}]
+        end
+      end
+
+      macro inherited
+        define_has_one
+      end
+    end
+
+    def mark_nested_save_operations_as_failed
+      nested_save_operations.each do |operation|
+        operation.as(Avram::MarkAsFailed).mark_as_failed
+      end
+    end
+
+    def nested_save_operations
+      [] of Avram::MarkAsFailed
+    end
+  end
+
   abstract class SaveOperation(T)
     include MailHelpers
+    include NewNestedSaveOperation
 
     def record!
       record.not_nil!
@@ -191,79 +292,6 @@ module Avram
     # for an update, independent of the stage we are at in the operation.
     def new_record? : Bool
       {{ T.constant(:PRIMARY_KEY_NAME).id }}.value.nil?
-    end
-  end
-
-  # Avram's implementation errors in an update operation:
-  #
-  # `duplicate key value violates unique constraint
-  # "<constraint name>" (PQ::PQError)`
-  #
-  # `{{ type }}.new(params)` causes `{{ name }}.save` to create
-  # (rather than update) a record each time it is called, since
-  # no record was passed when the nested operation was instantiated.
-  #
-  # Ref: https://github.com/luckyframework/avram/blob/efe1d8afaf337809c2accf51300daac48dba1cdc/src/avram/nested_save_operation.cr
-  module NestedSaveOperation
-    macro has_one(type_declaration)
-      {% name = type_declaration.var %}
-      {% type = type_declaration.type.resolve %}
-
-      {% model_type = type.ancestors.find do |t|
-           t.stringify.starts_with?("Avram::SaveOperation(")
-         end.type_vars.first %}
-
-      {% assoc = T.constant(:ASSOCIATIONS).find do |assoc|
-           assoc[:relationship_type] == :has_one &&
-             assoc[:type].resolve.name == model_type.name
-         end %}
-
-      {% unless assoc %}
-        {% raise "#{T} must have a has_one association with #{model_type}" %}
-      {% end %}
-
-      after_save save_{{ name }}
-
-      def save_{{ name }}(saved_record)
-        unless {{ name }}.save
-          add_error(:{{ name }}, "failed")
-          mark_nested_save_operations_as_failed
-          database.rollback
-        end
-      end
-
-      def {{ name }}
-        @{{ name }} ||= if new_record?
-          {{ type }}.new(params)
-        else
-          {{ type }}.new(record!.{{ assoc[:assoc_name].id }}!, params)
-        end
-
-        nested = @{{ name }}.not_nil!
-
-        record.try do |record|
-          nested.{{ @type.constant(:FOREIGN_KEY).id }}.value = record.id
-        end
-
-        nested
-      end
-
-      def nested_save_operations
-        {% if @type.methods.map(&.name).includes?(:nested_save_operations.id) %}
-          previous_def +
-        {% end %}
-        [{{ name }}]
-      end
-    end
-
-    def mark_nested_save_operations_as_failed
-      nested_save_operations.each do |operation|
-        operation.as(Avram::MarkAsFailed).mark_as_failed
-      end
-    end
-
-    def nested_save_operations
-      [] of Avram::MarkAsFailed
     end
   end
 
