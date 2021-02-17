095b7aeed1ea6036f67c7d574f0b49964eb51fd3
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2021-02-17 19:30:44 +0000

    Add array support for enum adapters created with `.__enum` macro

--- spec/charms_spec.cr
+++ spec/charms_spec.cr
@@ -21,9 +21,49 @@ describe Memoize do
 end
 
 describe ".__enum" do
-  it "works with queries" do
-    UserFactory.create &.level(:admin)
+  describe ".parse" do
+    it "parses enum" do
+      level = User::Level.new(:author)
+      User::Level.adapter.parse!(level).should eq(level)
+    end
 
-    UserQuery.new.level(:admin).first?.should be_a(User)
+    it "parses string" do
+      level = User::Level.new(:author)
+      User::Level.adapter.parse!("aUTHoR").should eq(level)
+    end
+
+    it "parses symbol" do
+      level = User::Level.new(:author)
+      User::Level.adapter.parse!(:aUTHoR).should eq(level)
+    end
+
+    it "parses array of enums" do
+      levels = [User::Level.new(:author)]
+      User::Level.adapter.parse!(levels).should eq(levels)
+    end
+
+    it "parses array of strings" do
+      levels = [User::Level.new(:author)]
+      User::Level.adapter.parse!(["author"]).should eq(levels)
+    end
+
+    it "parses array of symbols" do
+      levels = [User::Level.new(:author)]
+      User::Level.adapter.parse!([:author]).should eq(levels)
+    end
+
+    it "works with queries" do
+      UserFactory.create &.level(:admin)
+      UserQuery.new.level(:admin).first?.should be_a(User)
+    end
+  end
+
+  describe ".to_db" do
+    it "works" do
+      level = User::Level.new(:author)
+
+      User::Level.adapter.to_db!(level).should eq("Author")
+      User::Level.adapter.to_db!([level, level]).should eq("{Author,Author}")
+    end
   end
 end
