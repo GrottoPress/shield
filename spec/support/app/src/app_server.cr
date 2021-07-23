d9fb454d183db441e4b10a131b77615f69be4953
akadusei <attakusiadusei@gmail.com>
2021-07-23 16:56:26 +0000

    Remove unused/unneeded HTTP handlers

--- spec/support/app/src/app_server.cr
+++ spec/support/app/src/app_server.cr
@@ -1,18 +1,9 @@
 class AppServer < Lucky::BaseAppServer
   def middleware : Array(HTTP::Handler)
     [
-      # Lucky::ForceSSLHandler.new,
       Lucky::HttpMethodOverrideHandler.new,
-      # Lucky::LogHandler.new,
       Lucky::ErrorHandler.new(action: Errors::Show),
-      Lucky::RemoteIpHandler.new,
       Lucky::RouteHandler.new,
-      # Lucky::StaticCompressionHandler.new(
-      #   "./public",
-      #   file_ext: "gz",
-      #   content_encoding: "gzip"
-      # ),
-      # Lucky::StaticFileHandler.new("./public", false),
       Lucky::RouteNotFoundHandler.new,
     ] of HTTP::Handler
   end
