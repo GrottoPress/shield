GrottoPress/shield

commit 46a56385bce31ef01b2fa7d9a2063ba5131d6236

Rename `Shield::EmailConfirmation::Edit` to `Update`
Applies to its API counterpart as well.

src/shield/actions/email_confirmations/update.cr
@@ -1,4 +1,4 @@
-module Shield::Api::EmailConfirmations::Edit
+module Shield::EmailConfirmations::Update
   # IMPORTANT!
   #
   # This requires the user to be logged in to update their email address.
@@ -17,13 +17,13 @@ module Shield::Api::EmailConfirmations::Edit
 
     before :pin_email_confirmation_to_ip_address
 
-    # get "/email-confirmations/edit" do
+    # get "/email-confirmations/update" do
     #   run_operation
     # end
 
     def run_operation
-      EmailConfirmationParams.new(
-        params
+      EmailConfirmationSession.new(
+        session
       ).verify do |utility, email_confirmation|
         if email_confirmation.try &.user_id == user.id # <= IMPORTANT!
           update_email(email_confirmation.not_nil!)
@@ -34,28 +34,20 @@ module Shield::Api::EmailConfirmations::Edit
       end
     end
 
-    {% if Avram::Model.all_subclasses
-      .map(&.stringify)
-      .includes?("BearerLogin") %}
-
-      def user
-        current_user_or_bearer
-      end
-    {% else %}
-      def user
-        current_user
-      end
-    {% end %}
+    def user
+      current_user
+    end
 
     def do_verify_operation_failed(utility)
-      json({status: "failure", message: Rex.t(:"action.misc.token_invalid")})
+      flash.failure = Rex.t(:"action.misc.token_invalid")
+      redirect to: New
     end
 
     private def update_email(email_confirmation)
       UpdateConfirmedEmail.update(
         email_confirmation.user.not_nil!,
         email_confirmation: email_confirmation,
-        session: nil
+        session: session
       ) do |operation, updated_user|
         if operation.saved?
           do_run_operation_succeeded(operation, updated_user)
@@ -67,19 +59,13 @@ module Shield::Api::EmailConfirmations::Edit
     end
 
     def do_run_operation_succeeded(operation, user)
-      json({
-        status: "success",
-        message: Rex.t(:"action.email_confirmation.edit.success"),
-        data: {user: UserSerializer.new(user)}
-      })
+      flash.success = Rex.t(:"action.email_confirmation.edit.success")
+      redirect to: CurrentUser::Show
     end
 
     def do_run_operation_failed(operation)
-      json({
-        status: "failure",
-        message: Rex.t(:"action.email_confirmation.edit.failure"),
-        data: {errors: operation.errors}
-      })
+      flash.failure = Rex.t(:"action.email_confirmation.edit.failure")
+      redirect to: CurrentUser::Update
     end
 
     def authorize?(user : Shield::User) : Bool

spec/support/app/src/actions/api/email_confirmations/update.cr
@@ -1,0 +1,10 @@
+class Api::EmailConfirmations::Update < ApiAction
+  include Shield::Api::EmailConfirmations::Update
+
+  skip :pin_login_to_ip_address
+  skip :pin_email_confirmation_to_ip_address
+
+  get "/email-confirmations/update" do
+    run_operation
+  end
+end

spec/support/app/src/actions/email_confirmations/update.cr
@@ -1,10 +1,10 @@
-class EmailConfirmations::Edit < BrowserAction
-  include Shield::EmailConfirmations::Edit
+class EmailConfirmations::Update < BrowserAction
+  include Shield::EmailConfirmations::Update
 
   skip :pin_email_confirmation_to_ip_address
   skip :pin_login_to_ip_address
 
-  get "/email-confirmations/edit" do
+  get "/email-confirmations/update" do
     run_operation
   end
 

src/shield/actions/api/email_confirmations/update.cr
@@ -1,4 +1,4 @@
-module Shield::EmailConfirmations::Edit
+module Shield::Api::EmailConfirmations::Update
   # IMPORTANT!
   #
   # This requires the user to be logged in to update their email address.
@@ -17,13 +17,13 @@ module Shield::EmailConfirmations::Edit
 
     before :pin_email_confirmation_to_ip_address
 
-    # get "/email-confirmations/edit" do
+    # get "/email-confirmations/update" do
     #   run_operation
     # end
 
     def run_operation
-      EmailConfirmationSession.new(
-        session
+      EmailConfirmationParams.new(
+        params
       ).verify do |utility, email_confirmation|
         if email_confirmation.try &.user_id == user.id # <= IMPORTANT!
           update_email(email_confirmation.not_nil!)
@@ -34,20 +34,28 @@ module Shield::EmailConfirmations::Edit
       end
     end
 
-    def user
-      current_user
-    end
+    {% if Avram::Model.all_subclasses
+      .map(&.stringify)
+      .includes?("BearerLogin") %}
+
+      def user
+        current_user_or_bearer
+      end
+    {% else %}
+      def user
+        current_user
+      end
+    {% end %}
 
     def do_verify_operation_failed(utility)
-      flash.failure = Rex.t(:"action.misc.token_invalid")
-      redirect to: New
+      json({status: "failure", message: Rex.t(:"action.misc.token_invalid")})
     end
 
     private def update_email(email_confirmation)
       UpdateConfirmedEmail.update(
         email_confirmation.user.not_nil!,
         email_confirmation: email_confirmation,
-        session: session
+        session: nil
       ) do |operation, updated_user|
         if operation.saved?
           do_run_operation_succeeded(operation, updated_user)
@@ -59,13 +67,19 @@ module Shield::EmailConfirmations::Edit
     end
 
     def do_run_operation_succeeded(operation, user)
-      flash.success = Rex.t(:"action.email_confirmation.edit.success")
-      redirect to: CurrentUser::Show
+      json({
+        status: "success",
+        message: Rex.t(:"action.email_confirmation.edit.success"),
+        data: {user: UserSerializer.new(user)}
+      })
     end
 
     def do_run_operation_failed(operation)
-      flash.failure = Rex.t(:"action.email_confirmation.edit.failure")
-      redirect to: CurrentUser::Edit
+      json({
+        status: "failure",
+        message: Rex.t(:"action.email_confirmation.edit.failure"),
+        data: {errors: operation.errors}
+      })
     end
 
     def authorize?(user : Shield::User) : Bool

spec/shield/actions/api/email_confirmations/update_spec.cr
@@ -1,6 +1,6 @@
 require "../../../../spec_helper"
 
-describe Shield::Api::EmailConfirmations::Edit do
+describe Shield::Api::EmailConfirmations::Update do
   it "updates email confirmation user" do
     email = "user@example.tld"
     new_email = "user@domain.net"
@@ -22,7 +22,7 @@ describe Shield::Api::EmailConfirmations::Edit do
       client = ApiClient.new
       client.api_auth(user, password)
 
-      response = client.exec(Api::EmailConfirmations::Edit, token: token)
+      response = client.exec(Api::EmailConfirmations::Update, token: token)
 
       response.should send_json(200, {
         message: "action.email_confirmation.edit.success"
@@ -41,13 +41,16 @@ describe Shield::Api::EmailConfirmations::Edit do
     client = ApiClient.new
     client.api_auth(email, password)
 
-    response = client.exec(Api::EmailConfirmations::Edit, token: token)
+    response = client.exec(Api::EmailConfirmations::Update, token: token)
 
     response.should send_json(403, {status: "failure"})
   end
 
   it "requires logged in" do
-    response = ApiClient.exec(Api::EmailConfirmations::Edit, token: "1.abcdef")
+    response = ApiClient.exec(
+      Api::EmailConfirmations::Update,
+      token: "1.abcdef"
+    )
 
     response.should send_json(401, logged_in: false)
   end

spec/shield/actions/email_confirmations/update_spec.cr
@@ -1,6 +1,6 @@
 require "../../../spec_helper"
 
-describe Shield::EmailConfirmations::Edit do
+describe Shield::EmailConfirmations::Update do
   it "updates email confirmation user" do
     email = "user@example.tld"
     new_email = "user@domain.net"
@@ -23,7 +23,7 @@ describe Shield::EmailConfirmations::Edit do
       client = ApiClient.new
       client.browser_auth(user, password, session: session)
 
-      response = client.exec(EmailConfirmations::Edit)
+      response = client.exec(EmailConfirmations::Update)
 
       user.reload.email.should eq(new_email)
     end
@@ -39,14 +39,14 @@ describe Shield::EmailConfirmations::Edit do
     client = ApiClient.new
     client.browser_auth(email, password, session: session)
 
-    response = client.exec(EmailConfirmations::Edit)
+    response = client.exec(EmailConfirmations::Update)
 
     response.status.should eq(HTTP::Status::FOUND)
     response.headers["X-Email-Confirmation-Status"]?.should eq("failure")
   end
 
   it "requires logged in" do
-    response = ApiClient.exec(EmailConfirmations::Edit)
+    response = ApiClient.exec(EmailConfirmations::Update)
 
     response.status.should eq(HTTP::Status::FOUND)
     response.headers["X-Logged-In"].should eq("false")

spec/support/app/src/actions/api/email_confirmations/edit.cr
@@ -1,10 +1,0 @@
-class Api::EmailConfirmations::Edit < ApiAction
-  include Shield::Api::EmailConfirmations::Edit
-
-  skip :pin_login_to_ip_address
-  skip :pin_email_confirmation_to_ip_address
-
-  get "/email-confirmations/edit" do
-    run_operation
-  end
-end

src/shield/actions/email_confirmations/show.cr
@@ -29,7 +29,7 @@ module Shield::EmailConfirmations::Show
 
     private def redirect(email_confirmation_session)
       if email_confirmation_session.email_confirmation.try &.user_id
-        redirect to: EmailConfirmations::Edit
+        redirect to: EmailConfirmations::Update
       else
         redirect to: CurrentUser::New
       end

commit 1b37ab1a763d5105152338e2ae293f5874b7dbb0

Fix wrong redirection
Regression in commit 46a56385bce31ef01b2fa7d9a2063ba5131d6236.

src/shield/actions/email_confirmations/update.cr
@@ -65,7 +65,7 @@ module Shield::EmailConfirmations::Update
 
     def do_run_operation_failed(operation)
       flash.failure = Rex.t(:"action.email_confirmation.edit.failure")
-      redirect to: CurrentUser::Update
+      redirect to: CurrentUser::Edit
     end
 
     def authorize?(user : Shield::User) : Bool