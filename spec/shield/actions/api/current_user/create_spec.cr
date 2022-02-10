GrottoPress/shield

commit 620b59ad5915986a38855df5f658f60f70105d58

Respond with HTTP status code `400` in actions if operation failed

src/shield/actions/api/current_user/create.cr
@@ -17,6 +17,7 @@ module Shield::Api::CurrentUser::Create
         if operation.saved?
           do_run_operation_succeeded(operation, user.not_nil!)
         else
+          response.status_code = 400
           do_run_operation_failed(operation)
         end
       end

src/shield/actions/api/email_confirmations/create.cr
@@ -18,6 +18,7 @@ module Shield::Api::EmailConfirmations::Create
         if operation.saved?
           do_run_operation_succeeded(operation, email_confirmation.not_nil!)
         else
+          response.status_code = 400
           do_run_operation_failed(operation)
         end
       end

src/shield/actions/api/password_resets/create.cr
@@ -22,6 +22,7 @@ module Shield::Api::PasswordResets::Create
         if operation.saved?
           do_run_operation_succeeded(operation, password_reset.not_nil!)
         else
+          response.status_code = 400
           do_run_operation_failed(operation)
         end
       end

src/shield/actions/current_user/create.cr
@@ -17,6 +17,7 @@ module Shield::CurrentUser::Create
         if operation.saved?
           do_run_operation_succeeded(operation, user.not_nil!)
         else
+          response.status_code = 400
           do_run_operation_failed(operation)
         end
       end

src/shield/actions/email_confirmations/create.cr
@@ -18,6 +18,7 @@ module Shield::EmailConfirmations::Create
         if operation.saved?
           do_run_operation_succeeded(operation, email_confirmation.not_nil!)
         else
+          response.status_code = 400
           do_run_operation_failed(operation)
         end
       end

src/shield/actions/password_resets/create.cr
@@ -22,6 +22,7 @@ module Shield::PasswordResets::Create
         if operation.saved?
           do_run_operation_succeeded(operation, password_reset.not_nil!)
         else
+          response.status_code = 400
           do_run_operation_failed(operation)
         end
       end

src/shield/actions/api/bearer_logins/create.cr
@@ -16,6 +16,7 @@ module Shield::Api::BearerLogins::Create
         if operation.saved?
           do_run_operation_succeeded(operation, bearer_login.not_nil!)
         else
+          response.status_code = 400
           do_run_operation_failed(operation)
         end
       end

src/shield/actions/api/email_confirmation_current_user/create.cr
@@ -36,6 +36,7 @@ module Shield::Api::EmailConfirmationCurrentUser::Create
         if operation.saved?
           do_run_operation_succeeded(operation, user.not_nil!)
         else
+          response.status_code = 400
           do_run_operation_failed(operation)
         end
       end

commit 7ca75f416e7c14bf94fd00cdc63987d8ff22431b

Fix operation status leak via HTTP response status codes
Regression in commit 620b59ad5915986a38855df5f658f60f70105d58, which
effectively nullified the user enumeration mitigations of *Shield*.

src/shield/actions/api/current_user/create.cr
@@ -17,7 +17,7 @@ module Shield::Api::CurrentUser::Create
         if operation.saved?
           do_run_operation_succeeded(operation, user.not_nil!)
         else
-          response.status_code = 400
+          response.status_code = 400 unless operation.user_email?
           do_run_operation_failed(operation)
         end
       end

src/shield/actions/api/email_confirmations/create.cr
@@ -18,7 +18,7 @@ module Shield::Api::EmailConfirmations::Create
         if operation.saved?
           do_run_operation_succeeded(operation, email_confirmation.not_nil!)
         else
-          response.status_code = 400
+          response.status_code = 400 unless operation.user_email?
           do_run_operation_failed(operation)
         end
       end

src/shield/actions/api/password_resets/create.cr
@@ -22,7 +22,7 @@ module Shield::Api::PasswordResets::Create
         if operation.saved?
           do_run_operation_succeeded(operation, password_reset.not_nil!)
         else
-          response.status_code = 400
+          response.status_code = 400 unless operation.guest_email?
           do_run_operation_failed(operation)
         end
       end

src/shield/actions/current_user/create.cr
@@ -17,7 +17,7 @@ module Shield::CurrentUser::Create
         if operation.saved?
           do_run_operation_succeeded(operation, user.not_nil!)
         else
-          response.status_code = 400
+          response.status_code = 400 unless operation.user_email?
           do_run_operation_failed(operation)
         end
       end

src/shield/actions/email_confirmations/create.cr
@@ -18,7 +18,7 @@ module Shield::EmailConfirmations::Create
         if operation.saved?
           do_run_operation_succeeded(operation, email_confirmation.not_nil!)
         else
-          response.status_code = 400
+          response.status_code = 400 unless operation.user_email?
           do_run_operation_failed(operation)
         end
       end

src/shield/actions/password_resets/create.cr
@@ -22,7 +22,7 @@ module Shield::PasswordResets::Create
         if operation.saved?
           do_run_operation_succeeded(operation, password_reset.not_nil!)
         else
-          response.status_code = 400
+          response.status_code = 400 unless operation.guest_email?
           do_run_operation_failed(operation)
         end
       end

spec/shield/actions/api/current_user/create_spec.cr
@@ -20,6 +20,27 @@ describe Shield::Api::CurrentUser::Create do
     })
   end
 
+  it "succeeds even if email already taken" do
+    email = "user@example.tld"
+    password = "password4APASSWORD<"
+
+    user = UserFactory.create &.email(email)
+
+    response = ApiClient.exec(
+      Api::RegularCurrentUser::Create,
+      user: {email: email, password: password},
+      user_options: {
+        password_notify: true,
+        login_notify: true,
+        bearer_login_notify: true
+      }
+    )
+
+    response.should send_json(200, {
+      message: "action.current_user.create.success"
+    })
+  end
+
   it "requires logged out" do
     email = "user@example.tld"
     password = "password4APASSWORD<"

spec/shield/actions/api/email_confirmations/create_spec.cr
@@ -10,6 +10,22 @@ describe Shield::Api::EmailConfirmations::Create do
     response.should send_json(200, {message: "action.misc.dev_mode_skip_email"})
   end
 
+  it "succeeds even if email already taken" do
+    email = "user@domain.net"
+
+    user = UserFactory.create &.email(email)
+
+    response = ApiClient.exec(
+      Api::EmailConfirmations::Create,
+      email_confirmation: {email: email}
+    )
+
+    response.should send_json(
+      200,
+      {message: "action.email_confirmation.create.success"}
+    )
+  end
+
   it "requires logged out" do
     email = "user@example.tld"
     password = "password4APASSWORD<"

spec/shield/actions/api/password_resets/create_spec.cr
@@ -14,6 +14,19 @@ describe Shield::Api::PasswordResets::Create do
     response.should send_json(200, {message: "action.misc.dev_mode_skip_email"})
   end
 
+  it "succeeds even if email does not exist" do
+    email = "user@example.tld"
+
+    response = ApiClient.exec(Api::PasswordResets::Create, password_reset: {
+      email: email
+    })
+
+    response.should send_json(
+      200,
+      {message: "action.password_reset.create.success"}
+    )
+  end
+
   it "requires logged out" do
     email = "user@example.tld"
     password = "password4APASSWORD<"

spec/shield/actions/current_user/create_spec.cr
@@ -19,6 +19,26 @@ describe Shield::CurrentUser::Create do
     response.headers["X-User-ID"]?.should eq("current_user_id")
   end
 
+  it "succeeds even if email already taken" do
+    email = "user@example.tld"
+    password = "password4APASSWORD<"
+
+    user = UserFactory.create &.email(email)
+
+    response = ApiClient.exec(
+      RegularCurrentUser::Create,
+      user: {email: email, password: password},
+      user_options: {
+        password_notify: true,
+        login_notify: true,
+        bearer_login_notify: true
+      }
+    )
+
+    response.status.should eq(HTTP::Status::FOUND)
+    response.headers["X-User-Created"]?.should eq("true")
+  end
+
   it "requires logged out" do
     email = "user@example.tld"
     password = "password4APASSWORD<"

spec/shield/actions/email_confirmations/create_spec.cr
@@ -9,6 +9,20 @@ describe Shield::EmailConfirmations::Create do
     response.headers["X-Email-Confirmation-ID"].should eq("ec_id")
   end
 
+  it "succeeds even if email already taken" do
+    email = "user@domain.net"
+
+    user = UserFactory.create &.email(email)
+
+    response = ApiClient.exec(
+      EmailConfirmations::Create,
+      email_confirmation: {email: email}
+    )
+
+    response.status.should eq(HTTP::Status::FOUND)
+    response.headers["X-Email-Confirmation-Status"].should eq("success")
+  end
+
   it "requires logged out" do
     email = "user@example.tld"
     password = "password4APASSWORD<"

spec/shield/actions/password_resets/create_spec.cr
@@ -15,6 +15,17 @@ describe Shield::PasswordResets::Create do
     response.headers["X-Password-Reset-ID"].should eq("pr_id")
   end
 
+  it "succeeds even if email does not exist" do
+    email = "user@example.tld"
+
+    response = ApiClient.exec(PasswordResets::Create, password_reset: {
+      email: email
+    })
+
+    response.status.should eq(HTTP::Status::FOUND)
+    response.headers["X-Password-Reset-Status"].should eq("success")
+  end
+
   it "requires logged out" do
     email = "user@example.tld"
     password = "password4APASSWORD<"

spec/support/app/src/actions/email_confirmations/create.cr
@@ -9,4 +9,9 @@ class EmailConfirmations::Create < BrowserAction
     response.headers["X-Email-Confirmation-ID"] = "ec_id"
     previous_def
   end
+
+  private def success_action(operation)
+    response.headers["X-Email-Confirmation-Status"] = "success"
+    previous_def
+  end
 end

spec/support/app/src/actions/password_resets/create.cr
@@ -9,4 +9,9 @@ class PasswordResets::Create < BrowserAction
     response.headers["X-Password-Reset-ID"] = "pr_id"
     previous_def
   end
+
+  private def success_action(operation)
+    response.headers["X-Password-Reset-Status"] = "success"
+    previous_def
+  end
 end

spec/support/app/src/actions/regular_current_user/create.cr
@@ -19,4 +19,9 @@ class RegularCurrentUser::Create < BrowserAction
     response.headers["X-User-ID"] = "current_user_id"
     previous_def
   end
+
+  private def success_action(operation)
+    response.headers["X-User-Created"] = "true"
+    previous_def
+  end
 end