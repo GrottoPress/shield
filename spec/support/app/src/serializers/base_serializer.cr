944811f90b89230a1e9ef1cd9d92c874ee7db6ca
akadusei <attakusiadusei@gmail.com>
2022-02-25 12:29:55 +0000

    Add `BaseSerializer.for_collection` method
    Removed in <https://github.com/GrottoPress/lucille/commit/5ae5891ca7896ff6e9106e37c3b6e8a4530d8dee>.

--- spec/support/app/src/serializers/base_serializer.cr
+++ spec/support/app/src/serializers/base_serializer.cr
@@ -1,3 +1,7 @@
 abstract struct BaseSerializer
   include Lucille::Serializer
+
+  def self.for_collection(collection : Enumerable, *args, **named_args)
+    collection.map { |object| new(object, *args, **named_args) }
+  end
 end
