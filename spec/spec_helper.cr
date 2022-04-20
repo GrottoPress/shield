442f7e5486522a106a55567dab8cd5bebc004de4
akadusei <attakusiadusei@gmail.com>
2022-04-20 18:21:32 +0000

    Make additional *Lucky* v0.30 changes
    Related to commit b568f4a3455dbd09d132b492ffead18f44a63d1b.

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -11,6 +11,8 @@ require "../src/spec"
 include Carbon::Expectations
 include Lucky::RequestExpectations
 
+Avram::SpecHelper.use_transactional_specs(AppDatabase)
+
 Avram::Migrator::Runner.new.ensure_migrated!
 Avram::SchemaEnforcer.ensure_correct_column_mappings!
 Habitat.raise_if_missing_settings!
