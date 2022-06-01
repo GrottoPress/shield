daa4f19509b1c382c89d7f255a87d6701a5c6a0e
akadusei <attakusiadusei@gmail.com>
2022-06-01 10:04:06 +0000

    Disallow updating revoked bearer logins

--- spec/shield/operations/update_bearer_login_spec.cr
+++ spec/shield/operations/update_bearer_login_spec.cr
@@ -24,4 +24,23 @@ describe Shield::UpdateBearerLogin do
       updated_bearer_login.scopes.should eq(new_scopes)
     end
   end
+
+  it "ensures bearer login is active" do
+    user = UserFactory.create
+    UserOptionsFactory.create &.user_id(user.id)
+
+    bearer_login = BearerLoginFactory.create &.user_id(user.id)
+      .inactive_at(Time.utc)
+
+    UpdateBearerLogin.update(
+      bearer_login,
+      params(name: "super duper secret"),
+      scopes: ["current_user.show"],
+      allowed_scopes: ["posts.update", "current_user.show"]
+    ) do |operation, updated_bearer_login|
+      operation.saved?.should be_false
+
+      operation.name.should have_error("operation.error.bearer_login_inactive")
+    end
+  end
 end
--- src/shield/operations/update_bearer_login.cr
+++ src/shield/operations/update_bearer_login.cr
@@ -2,6 +2,17 @@ module Shield::UpdateBearerLogin
   macro included
     permit_columns :name
 
+    before_save do
+      validate_status_active
+    end
+
     include Shield::ValidateBearerLogin
+
+    private def validate_status_active
+      record.try do |bearer_login|
+        return if bearer_login.status.active?
+        name.add_error Rex.t(:"operation.error.bearer_login_inactive")
+      end
+    end
   end
 end
