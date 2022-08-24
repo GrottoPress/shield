87a323d47cef84bb8f34560d7c57fab208ac0140
akadusei <attakusiadusei@gmail.com>
2022-08-24 16:43:21 +0000

    Add OAuth client query methods

--- src/shield/queries/oauth_client_query.cr
+++ src/shield/queries/oauth_client_query.cr
@@ -1,5 +1,13 @@
 module Shield::OauthClientQuery
   macro included
     include Lucille::StatusQuery
+
+    def is_public
+      secret_digest.is_nil
+    end
+
+    def is_confidential
+      secret_digest.is_not_nil
+    end
   end
 end
