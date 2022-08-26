948c9172936684363047338742563be7a0409ecb
akadusei <attakusiadusei@gmail.com>
2022-08-26 11:30:25 +0000

    Undo skipped pipes in token creation action in specs

--- spec/support/app/src/actions/api/oauth/token/create.cr
+++ spec/support/app/src/actions/api/oauth/token/create.cr
@@ -1,17 +1,6 @@
 class Api::Oauth::Token::Create < ApiAction
   include Shield::Api::Oauth::Token::Create
 
-  skip :pin_login_to_ip_address
-  skip :oauth_validate_client_id
-  # skip :oauth_handle_errors
-  skip :oauth_check_duplicate_params
-  skip :oauth_require_access_token_params
-  skip :oauth_validate_redirect_uri
-  skip :oauth_validate_grant_type
-  skip :oauth_validate_code
-  skip :oauth_check_multiple_client_auth
-  skip :oauth_validate_client_secret
-
   post "/oauth/token" do
     run_operation
   end
