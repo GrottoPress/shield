8fa085383e481d1675447ae895213205ac7d3ac1
akadusei <attakusiadusei@gmail.com>
2022-09-26 20:49:02 +0000

    Expand `Shield::BearerLoginVerifier` specs
    Expanded to cover `Shield::OauthAccessTokenVerifier` methods.

--- spec/shield/utilities/mixins/bearer_login_verifier_spec.cr
+++ spec/shield/utilities/mixins/bearer_login_verifier_spec.cr
@@ -4,31 +4,49 @@ describe Shield::BearerLoginVerifier do
   describe "#verify" do
     it "verifies bearer login" do
       scope = BearerScope.new(Api::Posts::Index).to_s
+      token = "a1b2c3"
 
-      user = UserFactory.create
-      UserOptionsFactory.create &.user_id(user.id)
+      developer = UserFactory.create
+      oauth_client = OauthClientFactory.create &.user_id(developer.id)
+      oauth_client_2 = OauthClientFactory.create &.user_id(developer.id)
+      resource_owner = UserFactory.create &.email("resource@owner.com")
 
-      CreateBearerLogin.create(
-        params(name: "some token"),
-        user: user,
-        scopes: [scope],
-      ) do |operation, bearer_login|
-        bearer_login = bearer_login.not_nil!
+      bearer_login = BearerLoginFactory.create &.user_id(resource_owner.id)
+        .oauth_client_id(oauth_client.id)
+        .scopes([scope])
+        .token(token)
 
-        headers = HTTP::Headers.new
+      headers = HTTP::Headers.new
+      BearerLoginCredentials.new(token, bearer_login.id).authenticate(headers)
 
-        BearerLoginCredentials.new(operation, bearer_login)
-          .authenticate(headers)
+      headers_2 = HTTP::Headers.new
+      BearerLoginCredentials.new("abcdefghij", 1).authenticate(headers_2)
 
-        headers_2 = HTTP::Headers.new
-        BearerLoginCredentials.new("abcdefghij", 1).authenticate(headers_2)
+      BearerLoginHeaders.new(headers).verify(oauth_client, scope)
+        .should(be_a BearerLogin)
 
-        BearerLoginHeaders.new(headers).verify.should be_a(BearerLogin)
-        BearerLoginHeaders.new(headers).verify(scope).should be_a(BearerLogin)
+      BearerLoginHeaders.new(headers).verify(oauth_client)
+        .should(be_a BearerLogin)
 
-        BearerLoginHeaders.new(headers).verify("invalid").should be_nil
-        BearerLoginHeaders.new(headers_2).verify.should be_nil
-      end
+      BearerLoginHeaders.new(headers).verify(scope).should be_a(BearerLogin)
+      BearerLoginHeaders.new(headers).verify.should be_a(BearerLogin)
+
+      BearerLoginHeaders.new(headers).verify(oauth_client_2, scope)
+        .should(be_nil)
+
+      BearerLoginHeaders.new(headers).verify(oauth_client, "api").should(be_nil)
+
+      BearerLoginHeaders.new(headers).verify(oauth_client_2).should(be_nil)
+      BearerLoginHeaders.new(headers).verify("api").should(be_nil)
+
+      BearerLoginHeaders.new(headers_2).verify(oauth_client, scope)
+        .should(be_nil)
+
+      BearerLoginHeaders.new(headers_2).verify(oauth_client)
+        .should(be_nil)
+
+      BearerLoginHeaders.new(headers_2).verify(scope).should(be_nil)
+      BearerLoginHeaders.new(headers_2).verify.should(be_nil)
     end
   end
 end
