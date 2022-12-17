101cb179c60a27b1f63f7c188aebb6e88b8ec160
akadusei <attakusiadusei@gmail.com>
2022-12-17 11:15:10 +0000

    Fix compile error in a consumer app:
    ```
    Error: undefined local variable or method 'bearer_scope' for
    CurrentUser::Apps::Index
    ```

--- src/shield/actions/api/mixins/login_helpers.cr
+++ src/shield/actions/api/mixins/login_helpers.cr
@@ -5,5 +5,37 @@ module Shield::Api::LoginHelpers
     getter? current_login : Login? do
       LoginHeaders.new(context).verify
     end
+
+    def bearer_logged_in? : Bool
+      !bearer_logged_out?
+    end
+
+    def bearer_logged_out? : Bool
+      current_bearer?.nil?
+    end
+
+    def current_user_or_bearer : User
+      current_user_or_bearer?.not_nil!
+    end
+
+    def current_user_or_bearer? : User?
+      current_user? || current_bearer?
+    end
+
+    def current_bearer : User
+      current_bearer?.not_nil!
+    end
+
+    getter? current_bearer : User? do
+      current_bearer_login?.try(&.user)
+    end
+
+    def current_bearer_login
+      current_bearer_login?.not_nil!
+    end
+
+    def current_bearer_login?
+      nil
+    end
   end
 end
