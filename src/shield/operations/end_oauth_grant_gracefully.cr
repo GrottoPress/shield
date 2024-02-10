3a48c056d1ed76ceb2c7c1f0ffb77aba0d89b05b
akadusei <attakusiadusei@gmail.com>
2024-02-10 11:43:39 +0000

    Simplify `EndOauthGrantGracefully` operation
    Avoids a nested `EndOauthGrant` operation for setting `inactive_at`.

--- src/shield/operations/end_oauth_grant_gracefully.cr
+++ src/shield/operations/end_oauth_grant_gracefully.cr
@@ -1,16 +1,17 @@
 module Shield::EndOauthGrantGracefully
   macro included
-    after_save end_oauth_grant
+    include Lucille::Deactivate
 
-    private def end_oauth_grant(oauth_grant : OauthGrant)
-      inactive_time = oauth_grant.type.refresh_token? ?
-        Time.utc + Shield.settings.oauth_refresh_token_grace :
-        Time.utc
+    private def set_inactive_at
+      record.try do |oauth_grant|
+        return if oauth_grant.status.inactive? || oauth_grant.status.unactive?
 
-      self.record = EndOauthGrant.update!(
-        oauth_grant,
-        inactive_at: inactive_time
-      )
+        inactive_at.value = oauth_grant.type.refresh_token? ?
+          Time.utc + Shield.settings.oauth_refresh_token_grace :
+          Time.utc
+
+        previous_def
+      end
     end
   end
 end
