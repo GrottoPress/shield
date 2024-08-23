528f05376be2010eecdaa241d3c53214f61a211f
akadusei <attakusiadusei@gmail.com>
2024-08-23 00:17:09 +0000

    Fix compile error
    Tested in [Samba](https://github.com/grottopress/samba):
    
    ```
    Showing last frame. Use --error-trace for full trace.
    
    There was a problem expanding macro 'included'
    
    Code in macro 'included'
    
     20 | include Shield::NotifyOauthAccessTokenIfSet
          ^
    Called macro defined in lib/shield/src/shield/operations/mixins/notify_oauth_access_token_if_set.cr:2:3
    
     2 | macro included
    
    Which expanded to:
    
     > 5 |       bearer_login = BearerLoginQuery.preload_oauth_client(bearer_login)
     > 6 |
     > 7 |       return unless bearer_login.user.settings.oauth_access_token_notify?
                                                          ^-------------------------
    Error: undefined method 'oauth_access_token_notify?' for UserSettings
    ```

--- src/shield/models/user_settings.cr
+++ src/shield/models/user_settings.cr
@@ -2,15 +2,15 @@ module Shield::UserSettings
   macro included
     include Lucille::JSON
 
-    {% if Lucille::JSON.includers.find(&.name.== :BearerLogin.id) %}
+    {% if Avram::Model.all_subclasses.find(&.name.== :BearerLogin.id) %}
       include Shield::BearerLoginUserSettings
     {% end %}
 
-    {% if Lucille::JSON.includers.find(&.name.== :Login.id) %}
+    {% if Avram::Model.all_subclasses.find(&.name.== :Login.id) %}
       include Shield::LoginUserSettings
     {% end %}
 
-    {% if Lucille::JSON.includers.find(&.name.== :OauthClient.id) %}
+    {% if Avram::Model.all_subclasses.find(&.name.== :OauthClient.id) %}
       include Shield::OauthClientUserSettings
     {% end %}
 
