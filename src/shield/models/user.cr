e2491888d4ef1141f6260e194c8d9fce414e3aa2
akadusei <attakusiadusei@gmail.com>
2024-09-11 15:01:33 +0000

    Remove model association presets
    Fixes compile error resulting from including the same mixin twice.

--- src/shield/models/user.cr
+++ src/shield/models/user.cr
@@ -1,34 +1,14 @@
 module Shield::User
   macro included
-    {% if Avram::Model.all_subclasses.find(&.name.== :BearerLogin.id) %}
-      include Shield::HasManyBearerLogins
-    {% end %}
+    # include Shield::HasManyBearerLogins
+    # include Shield::HasManyEmailConfirmations
+    # include Shield::HasManyLogins
+    # include Shield::HasManyOauthClients
+    # include Shield::HasManyOauthGrants
+    # include Shield::HasManyPasswordResets
 
-    {% if Avram::Model.all_subclasses.find(&.name.== :EmailConfirmation.id) %}
-      include Shield::HasManyEmailConfirmations
-    {% end %}
-
-    {% if Avram::Model.all_subclasses.find(&.name.== :Login.id) %}
-      include Shield::HasManyLogins
-    {% end %}
-
-    {% if Avram::Model.all_subclasses.find(&.name.== :OauthClient.id) %}
-      include Shield::HasManyOauthClients
-    {% end %}
-
-    {% if Avram::Model.all_subclasses.find(&.name.== :OauthGrant.id) %}
-      include Shield::HasManyOauthGrants
-    {% end %}
-
-    {% if Avram::Model.all_subclasses.find(&.name.== :PasswordReset.id) %}
-      include Shield::HasManyPasswordResets
-    {% end %}
-
-    {% if Avram::Model.all_subclasses.find(&.name.== :UserOptions.id) %}
-      include Shield::HasOneUserOptions
-    {% elsif Lucille::JSON.includers.find(&.name.== :UserSettings.id) %}
-      include Shield::UserSettingsColumn
-    {% end %}
+    # include Shield::HasOneUserOptions
+    # #include Shield::UserSettingsColumn
 
     column email : String
     column password_digest : String
