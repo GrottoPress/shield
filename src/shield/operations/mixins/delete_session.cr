5e591c29b719ed922ee16cb55c665f4ac835b6b7
akadusei <attakusiadusei@gmail.com>
2024-10-23 16:05:44 +0000

    Use `after_commit` callback for delete operations

--- src/shield/operations/mixins/delete_session.cr
+++ src/shield/operations/mixins/delete_session.cr
@@ -5,7 +5,11 @@ module Shield::DeleteSession
     {% if @type < Avram::SaveOperation %}
       after_commit delete_session
     {% elsif @type < Avram::DeleteOperation %}
-      after_delete delete_session
+      {% if compare_versions(Avram::VERSION, "1.3.0") >= 0 %}
+        after_commit delete_session
+      {% else %}
+        after_delete delete_session
+      {% end %}
     {% elsif @type < Avram::Operation %}
       after_run delete_session
     {% end %}
--- src/shield/operations/mixins/set_session.cr
+++ src/shield/operations/mixins/set_session.cr
@@ -5,7 +5,11 @@ module Shield::SetSession
     {% if @type < Avram::SaveOperation %}
       after_commit set_session
     {% elsif @type < Avram::DeleteOperation %}
-      after_delete set_session
+      {% if compare_versions(Avram::VERSION, "1.3.0") >= 0 %}
+        after_commit set_session
+      {% else %}
+        after_delete set_session
+      {% end %}
     {% elsif @type < Avram::Operation %}
       after_run set_session
     {% end %}
